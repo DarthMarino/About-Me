cef0d1b06be5d013183a667cb40d04c54378a4a5
Marino <marinogomez24@gmail.com>
2020-03-12 12:54:38 -0400

    initial CV commit

---
+++ home.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hidencontent').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade in */
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},500);
+            }  
+        }); 
+    });
+});
+
+$(function() {
+          
+    var documentEl = $(document),
+        fadeElem = $('.fade-scroll');
+    
+    documentEl.on('scroll', function() {
+        var currScrollPos = documentEl.scrollTop();
+        
+        fadeElem.each(function() {
+            var $this = $(this),
+                elemOffsetTop = $this.offset().top;
+            if (currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/400);
+        }); 
+    });
+    
+});
